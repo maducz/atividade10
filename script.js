7bb93015f39379cead1ffd3ea82860f3f881f6d7
maducz <160507829+maducz@users.noreply.github.com>
2024-03-11 11:49:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function teste(){
+    alert("JS maravilhoso vitaminado funcionando");
+}
+
+function calcula(){
+    //criar variavel
+    let num1;
+    //recebe texto do html
+    num1=document.getElementById("numero1").value;
+    //transforma texto em numero real
+    let num1real=parseFloat(num1);
+
+    let num2;
+    num2=document.getElementById("numero2").value;
+    //transformar texto para numero real
+    let num2real=parseFloat(num2);
+
+    let result;
+    result=num1real+num2real;//atualizar nomes variaveis
+
+    document.getElementById("resultado").innerHTML=result;
+}
